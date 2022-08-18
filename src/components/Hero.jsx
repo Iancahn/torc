db0516d97cae81d197730c73ab112cf4c5052d8f
Ian Cahn <iancahn@live.com>
2022-08-18 15:11:48 +0200

    Hero Section Done
    Content and Styling done. Might need small tweaks.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import heroImage from '../img/hero-image.png'
+import heroImage from '../img/hero-image.png';
+import heroArrow from '../img/hero-arrow.png';
 
 function Header() {
     return (
@@ -14,6 +15,12 @@ function Header() {
                     <div className="column">
                         <img src={heroImage} alt=" the circle, squares, triangle and much more" id="hero-image" />
                     </div>
+                </div>
+                <div className="scrollToRead">
+                    <p>Scroll to read my articles</p>
+                </div>
+                <div className="scrollArrow">
+                    <img src={heroArrow} alt="an arrow pointing towards articles" id="scrollArrow" />
                 </div>
             </section >
         </div >
